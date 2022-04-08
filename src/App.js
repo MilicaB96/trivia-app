04b9727429ac693e10fe5c0ed117e324e55afdf8
Milica <magiflowerchild@gmial.com>
2022-04-08 18:25:36 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./App.css";
+import TriviaApp from "./pages/TriviaApp";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Router>
+        <nav className='navbar-brand'>
+          <ul>
+            <li>
+              <Link to='/'>TriviaApp</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route to='/'>
+            <TriviaApp />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
